ce1bba19ec3b807df9c7fd7a1a812faed46b12e5
Иван Локтаев <ivanloktaev@Da-Joey.local>
2018-11-17 18:24:26 +0300

    update

--- Problem 5.R
+++ Problem 5.R
@@ -1,2 +1,6 @@
-setwd("/Users/darafilyaskina/Desktop/notifications")
+#setwd("/Users/darafilyaskina/Desktop/notifications")
+setwd("/Users/ivanloktaev/Yandex.Disk.localized/Uni/4\ Курс/Data\ Science/ha1\ materials/hw1p5/notifications")
+library(XML)
+library(stringr)
 huinia<-htmlTreeParse(unzip("notification__Adygeja_Resp_inc_20110101_000000_20110201_000000_2.xml.zip"))
+huinia$children$html[1]
