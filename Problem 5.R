7fbd1a42cb156d45a4dbc7a5668143bb35b91015
Иван Локтаев <ivanloktaev@Da-Joey.local>
2018-11-18 23:17:43 +0300

    Final Changes

--- Problem 5.R
+++ Problem 5.R
@@ -3,14 +3,21 @@ setwd("/Users/darafilyaskina/Desktop/notifications")
 #setwd("/Users/ivanloktaev/Yandex.Disk.localized/Uni/4\ Курс/Data\ Science/ha1\ materials/hw1p5/notifications")
 library(XML)
 library(stringr)
-huinia<-xmlParse(unzip("notification__Adygeja_Resp_inc_20110101_000000_20110201_000000_2.xml.zip"))
+xml_file<-xmlParse(unzip("notification__Adygeja_Resp_inc_20110101_000000_20110201_000000_2.xml.zip"))
 
-root <- xmlRoot(huinia)
+root <- xmlRoot(xml_file)
 names(root[[1]][[2]])
 
 #create a dataframe of all nodes
 
-x<-xmlToDataFrame(root)
+x<-xmlToDataFrame(root, stringsAsFactors = FALSE)
 
 ## extract only those nodes that are needed
 xmlChildren(root)
+# <<<<<<< HEAD
+# =======
+library(data.table)
+x<-as.data.table(x)
+x[,placingWay.name:=lapply("placingWay",substr(as.character(x[1,5]),1,2))]
+  
+# >>>>>>> 3429b8f5e5961c72412cecb92c868f3ffd9fb702
